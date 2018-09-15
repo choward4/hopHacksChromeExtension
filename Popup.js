6f81ffa14a71028d27b9f273e69bcb5eb3bba02b
crazysaxer <chrishoward1337@gmail.com>
2018-09-15 11:52:09 -0400

    outline of event listeners between popup and content

--- Popup.js
+++ Popup.js
@@ -1,3 +1,13 @@
 function newTabs() {
-    document.addEventListener(click).write("hello world")
+    document.write("hello world");
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('tabBtn').addEventListener('click', newTabs);
+});
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+      console.log(response.farewell);
+    });
+  });
