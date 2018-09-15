53c9fa8c3292efcadf80ac7bae4ee06cbe3f4719
victoria-sauder <vsauder@umd.edu>
2018-09-15 12:23:33 -0400

    fancy html n slider

--- Popup.js
+++ Popup.js
@@ -1,3 +1,16 @@
 function newTabs() {
     document.addEventListener(click).write("hello world")
+    console.log("log test")
+}
+
+/* Display current value of slider */
+window.onload = function getValue() {
+    var sliderVal = document.getElementById("numArticlesSlider")
+    var valueOut = document.getElementById("currVal")
+    valueOut.innerText = sliderVal.value
+
+    sliderVal.oninput = function () {
+        valueOut.innerText = this.value
+    }
+
 }
