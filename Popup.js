4ff77dc5663cb4e9eee75b0e361a8828b97b7cd9
crazysaxer <chrishoward1337@gmail.com>
2018-09-15 15:58:05 -0400

    undid changes

--- Popup.js
+++ Popup.js
@@ -1,21 +1,11 @@
 function newTabs() {
     chrome.tabs.getSelected(null, function (tab) {
-        count = 0;
         var sitesVisited = "";
         var searchString = tab.title;
         var searchTerms = searchString.replace(" ", "+");
-        numTabs = getValue();
-        while(count < numTabs) {
-            // searchTerm+-site%3Afoxnews.com+-site%3Acnn.com
-            // "&btnI=I&as_sitesearch=twitter.com" old way of doing it
-            var myURL = "https://www.google.com/webhp?#q=" + searchTerms + "&btnI=I&" + sitesVisited;
-            chrome.tabs.create({ url: myURL });
-            //find way to obtain site last visited
-            site = "site.com"
-            //subtract site visited
-            site = "+-" + site
-        }
-        
+
+        var myURL = "https://www.google.com/webhp?#q=" + searchTerms + "&btnI=I";
+        chrome.tabs.create({ url: myURL });
     });
 }
 
